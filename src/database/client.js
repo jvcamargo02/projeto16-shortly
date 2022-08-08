22fd0b920eb7f1c88bd1db1e6b9c5674c1f3ce09
jvcamargo02 <joaovcamargo2002@gmail.com>
2022-08-08 10:56:35 -0300

    Chore: prepare for deploy

--- src/database/client.js
+++ src/database/client.js
@@ -6,11 +6,10 @@ dotenv.config()
 const { Pool } = pg;
 
 const connection = new Pool({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    connectionString: process.env.DB_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 export { connection as clientDb };
--- src/server.js
+++ src/server.js
@@ -15,6 +15,3 @@ app.use(router);
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(`Servidor is running on port ${PORT}`));
-
-
-/* SELECT   json_build_array(urls) as teste, u.id, u.name, SUM("visitCount") FROM users u JOIN urls ON u.id = urls."userId" WHERE u.id = 4 GROUP BY (u.id, urls.*) */
